12829c1b06a6981e448fdb2259ae6f8897a2bc54
Nick Radocaj <73852139+nkrad@users.noreply.github.com>
2020-12-31 23:49:35 -0600

    Add files via upload

---
+++ version_2/vanitykeygen.py
@@ -0,0 +1,40 @@
+import os
+import binascii
+import base58
+import hashlib
+from bitcoin import privtopub, pubtoaddr
+
+vanity = "1" + input("Enter a word you want your BTC address to start with(case sensitive): ")
+print("\nSearching for address starting with the word " + vanity + "\n")
+
+btcAddress = ""
+
+while btcAddress[:len(vanity)] != vanity:
+    privateKeyByte = binascii.hexlify(os.urandom(32))
+    privateKey = str(privateKeyByte)[2:-1]
+    extendedPrivateKey = ("80" + privateKey)
+    extendedPrivateKeyByte = bytearray.fromhex(extendedPrivateKey)
+    firstHash = hashlib.sha256()
+    firstHash.update(extendedPrivateKeyByte)
+    secondHash = hashlib.sha256()
+    firstHashFromHex = bytearray.fromhex(firstHash.hexdigest())
+    secondHash.update(firstHashFromHex)
+    checksum = secondHash.hexdigest()[0:8]
+    finalPrivateKey = extendedPrivateKey + checksum
+    encodedKey = bytes.fromhex(finalPrivateKey)
+    wifKey = base58.b58encode(encodedKey)
+    wifKeyString = str(wifKey)[2:-1]
+    publicKey = privtopub(privateKey)
+    btcAddress = pubtoaddr(publicKey)
+    keyURL = "https://www.blockchain.com/btc/address/" + btcAddress
+    print(btcAddress)
+
+print("\nYour private key is:                            " + privateKey)
+print("Your extended private key is:                   " + extendedPrivateKey)
+print ("Your private key hashed using SHA256 is:        " + firstHash.hexdigest())
+print("Your private key hashed using SHA256 again is:  " + secondHash.hexdigest())    
+print("Your private key in WIF is:                     " + wifKeyString)
+print ("Your Bitcoin address is:                        " + btcAddress)
+print("View address on the blockchain:                 " + keyURL)
+
+keyPairCount = input("")
